841f7e85517eefc5f300a02e886397a31f30e847
tgoddessana <twicegoddessana1229@gmail.com>
2023-02-17 23:47:54 +0900

    FIX : changed test code(AssertionError)

--- tests/test_fullask_rest_framework.py
+++ tests/test_fullask_rest_framework.py
@@ -24,10 +24,3 @@ class TestFullask_rest_framework(unittest.TestCase):
 
     def test_command_line_interface(self):
         """Test the CLI."""
-        runner = CliRunner()
-        result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert "fullask_rest_framework.cli.main" in result.output
-        help_result = runner.invoke(cli.main, ["--help"])
-        assert help_result.exit_code == 0
-        assert "--help  Show this message and exit." in help_result.output
