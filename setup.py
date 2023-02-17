489d82713c647935fccc9ac2f44bc10d73751616
tgoddessana <twicegoddessana1229@gmail.com>
2023-02-17 23:53:57 +0900

    FIX : changed python test settings

--- setup.py
+++ setup.py
@@ -19,14 +19,15 @@ setup(
     author_email="twicegoddessana1229@gmail.com",
     python_requires=">=3.8",
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: Pre-Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     description="A fully-supported flask extension to build REST API.",
     entry_points={
