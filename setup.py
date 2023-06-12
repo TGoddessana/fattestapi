f1dec2b7a63b308c2a8856587fc69ccd65764881
tgoddessana <twicegoddessana1229@gmail.com>
2023-06-12 23:58:32 +0900

    DEPLOY : Fixed UUIDMixin always generating the same value

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/tgoddessana/fullask-rest-framework",
-    version="0.1.3",
+    version="0.1.4",
     zip_safe=False,
 )
