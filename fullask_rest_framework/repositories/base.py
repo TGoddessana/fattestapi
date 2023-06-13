5650dfae16f75e1559de343ce159251b7ec07602
tgoddessana <twicegoddessana1229@gmail.com>
2023-06-14 02:40:03 +0900

    REFACTOR : removed entity as __init__ parameter of base repository. (#4)

--- fullask_rest_framework/repositories/base.py
+++ fullask_rest_framework/repositories/base.py
@@ -1,4 +1,4 @@
-from typing import Generic, Type, TypeVar
+from typing import Generic, TypeVar
 
 from fullask_rest_framework.entities.base_entity import BaseEntity
 
@@ -9,9 +9,3 @@ class BaseRepository(Generic[T]):
     """
     The Base Repository class of all Repositories.
     """
-
-    def __init__(self, entity: Type[T]):
-        """
-        :param entity: The types of entities you want to cover in this repository.
-        """
-        self.entity = entity
