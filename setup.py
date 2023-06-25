8919d51e9b1984d8072b0162e0e63c979b2ff50b
tgoddessana <twicegoddessana1229@gmail.com>
2023-06-25 19:04:50 +0900

    Bump version: 0.1.16 → 0.1.17

--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/tgoddessana/fullask-rest-framework",
-    version="0.1.16",
+    version="0.1.17",
     zip_safe=False,
 )
