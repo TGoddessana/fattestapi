20a83c201477360cde732dbda49741b4d962a617
tgoddessana <twicegoddessana1229@gmail.com>
2023-07-17 02:02:38 +0900

    refactor: remove `Metadata` from `db` object.

--- fullask_rest_framework/factory/extensions.py
+++ fullask_rest_framework/factory/extensions.py
@@ -2,21 +2,12 @@ from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from flask_marshmallow import Marshmallow  # type: ignore[import]
 from flask_migrate import Migrate
+from flask_smorest import Api
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
 
-db = SQLAlchemy(
-    metadata=MetaData(
-        naming_convention={
-            "ix": "ix_%(column_0_label)s",
-            "uq": "uq_%(table_name)s_%(column_0_name)s",
-            "ck": "ck_%(table_name)s_%(column_0_name)s",
-            "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-            "pk": "pk_%(table_name)s",
-        }
-    )
-)
+smorest = Api()
+db = SQLAlchemy()
+migrate = Migrate(db=db)
 cors = CORS()
 ma = Marshmallow()
 jwt = JWTManager()
-migrate = Migrate(db=db)
