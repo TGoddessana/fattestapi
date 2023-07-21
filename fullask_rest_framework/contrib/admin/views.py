fdc71f439d777dfca4a38ff48e72b44c541bea40
tgoddessana <twicegoddessana1229@gmail.com>
2023-07-22 00:12:50 +0900

    revert: Remove the Admin package, which will later be integrated with Flask-Admin.

--- fullask_rest_framework/contrib/admin/views.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-admin_bp = Blueprint("AdminPage", __name__, template_folder="./templates")
-
-
-@admin_bp.get("/")
-def admin_index():
-    return render_template("index.html")
